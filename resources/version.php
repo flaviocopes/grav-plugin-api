4effec91304bff5a0dcdf88c2e79ab21d8ff3930
Flavio Copes <copesc@gmail.com>
2015-11-01 00:01:05 +0100

    Implement actual version retrieval

--- resources/version.php
+++ resources/version.php
@@ -2,32 +2,65 @@
 namespace Grav\Plugin\Api;
 require_once 'resource.php';
 
+use Symfony\Component\Yaml\Yaml;
+use RocketTheme\Toolbox\File\File;
+
 /**
  * Version API
  */
 class Version extends Resource
 {
     /**
+     * Get API Version
      *
+     * Implements:
+     *
+     * - GET /api/version
      */
     public function getList()
     {
-        return '0';
+        $version = $this->getVersion();
+        return strtok($version, '.');
     }
+
     /**
+     * Implements:
      *
+     * - GET /api/version/major
+     * - GET /api/version/minor
+     * - GET /api/version/full
      */
     public function getItem()
     {
         switch($this->getIdentifier()) {
             case 'major':
-                return '0';
+                // Get API Major Version
+                $version = $this->getVersion();
+                return strtok($version, '.');
             case 'minor':
-                return '0.1';
+                // Get API Minor Version
+                $version = $this->getVersion();
+                return strtok($version, '.') . '.' . strtok('.');
             case 'full':
-                return '0.1.0';
+                //Get API Full Version
+                return $this->getVersion();
         }
 
         return;
+    }
+
+    /**
+     * Fetch the plugin version from the blueprints.yaml file
+     */
+    private function getVersion()
+    {
+        $locator = $this->grav['locator'];
+        $path = $locator->findResource('user://plugins/api', true);
+        $fullFileName = $path . DS . 'blueprints.yaml';
+
+        $file = File::instance($fullFileName);
+        $data = Yaml::parse($file->content());
+
+        return $data['version'];
     }
 }
